e61b9ba494fbbb134e511c82395641adb8345df3
Maria Concepcion Rodriguez aka Connie and Neil Bateman <maria@>
2019-02-11 20:24:03 -0800

    modify files

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,14 @@ $(document).ready(function() {
   event.preventDefault();
 
   var nameInput = $("input#name").val();
-
+  var sexInput = $("#sex").val();
+  var orientationInput = $("input:radio[name=sexual-orientation]:checked").val();
 
   console.log("The name is: " + nameInput );
   alert(nameInput);
   $('.name').text(nameInput);
+  $('.sex').text(sexInput);
+  $('.radio').text(orientationInput);
   $('#story').show();
   });
 });
